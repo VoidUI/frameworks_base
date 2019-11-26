298f498e116dae951254fe0fadb2f10c6e6e76ae
Christian Oder <myself5@carbonrom.org>
2019-11-26 23:35:01 +0100

    [SQUASH] base: SystemUI: Introduce DataSwitchTile
    * Based on OnePlus' OxygenOS tile, reworked to work with AOSP toggling without requirements on proprietary telephony-ext features
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    
    DataSwitchTile: Improve the code
    
    * Get rid of deprecated AsyncTask
    * Removed unused imports
    * Finalize global vars
    * Some other formatting and minor stuff
    
    Change-Id: Id3e39c98dac6200301d15bf752f866f47875633d
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    
    SystemUI: DataSwitchTile: improve the code once again
    
    * made the BroadcastReceiver private and final
    * made the PhoneStateListener an anonymous inner class
    * guard debug logs
    * preloaded some strings and variables that are likely to be used over and over
    * use the stream apis to handle subscription info list
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    * @jhonboy121: adapt to A12 and SysUIToast -> Toast
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I9a842de26baf6f415570a063a8efbcdedf11babe

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.CPUInfoTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
+import com.android.systemui.qs.tiles.DataSwitchTile;
 import com.android.systemui.qs.tiles.DeviceControlsTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
@@ -117,6 +118,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<VpnTile> mVpnTileProvider;
     private final Provider<CPUInfoTile> mCPUInfoTileProvider;
     private final Provider<FPSInfoTile> mFPSInfoTileProvider;
+    private final Provider<DataSwitchTile> mDataSwitchTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -164,7 +166,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<AntiFlickerTile> antiFlickerTileProvider,
             Provider<VpnTile> vpnTileProvider,
             Provider<CPUInfoTile> cpuInfoTileProvider,
-            Provider<FPSInfoTile> fpsInfoTileProvider) {
+            Provider<FPSInfoTile> fpsInfoTileProvider,
+            Provider<DataSwitchTile> dataSwitchTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -208,6 +211,7 @@ public class QSFactoryImpl implements QSFactory {
         mVpnTileProvider = vpnTileProvider;
         mCPUInfoTileProvider = cpuInfoTileProvider;
         mFPSInfoTileProvider = fpsInfoTileProvider;
+        mDataSwitchTileProvider = dataSwitchTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -301,6 +305,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mCPUInfoTileProvider.get();
             case "fpsinfo":
                 return mFPSInfoTileProvider.get();
+            case "dataswitch":
+                return mDataSwitchTileProvider.get();
         }
         // Custom tiles
         if (tileSpec.startsWith(CustomTile.PREFIX)) {
