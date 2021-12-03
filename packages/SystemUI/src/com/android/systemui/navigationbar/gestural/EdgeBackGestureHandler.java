9db2c78d38b490edded836551a70656b5daccf11
ezio84 <brabus84@gmail.com>
2021-12-03 19:51:29 +0530

    base: add API for disabling gestural navigation
    Change-Id: I4b76a803454af570c5945c920881335a5c4a8771

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -311,6 +311,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
         }
     };
 
+    private boolean mBlockedGesturalNavigation;
 
     EdgeBackGestureHandler(Context context, OverviewProxyService overviewProxyService,
             SysUiState sysUiState, PluginManager pluginManager, @Main Executor executor,
@@ -480,6 +481,10 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
         mIsNavBarShownTransiently = isTransient;
     }
 
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        mBlockedGesturalNavigation = blocked;
+    }
+
     private void disposeInputChannel() {
         if (mInputEventReceiver != null) {
             mInputEventReceiver.dispose();
@@ -542,7 +547,11 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                     "edge-swipe", mDisplayId);
             mInputEventReceiver = new InputChannelCompat.InputEventReceiver(
                     mInputMonitor.getInputChannel(), Looper.getMainLooper(),
-                    Choreographer.getInstance(), this::onInputEvent);
+                    Choreographer.getInstance(), event -> {
+                        if (!mBlockedGesturalNavigation) {
+                            onInputEvent(event);
+                        }
+                    });
 
             // Add a nav bar panel window
             setEdgeBackPlugin(new NavigationBarEdgePanel(mContext));
