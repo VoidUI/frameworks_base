4c176a1abcec0f96636e6c45a6d61e289415328d
Steven Moreland <smoreland@google.com>
2022-05-18 00:06:14 +0000

    Parcel: better debug recycle
    Detect recycle called twice always (and when DEBUG_RECYCLE is on, we
    detect recycle called twice or called zero times, and we show a stack).
    
    Bug: 231799394
    Test: manual (calling recycle twice)
    Change-Id: I1dd9f392ee916edd9c598085a1c19dbdd3ce957f
    Merged-In: I1dd9f392ee916edd9c598085a1c19dbdd3ce957f

--- core/java/android/os/Parcel.java
+++ core/java/android/os/Parcel.java
@@ -216,6 +216,7 @@ public final class Parcel {
     private ArrayMap<Class, Object> mClassCookies;
 
     private RuntimeException mStack;
+    private boolean mRecycled = false;
 
     /**
      * Whether or not to parcel the stack trace of an exception. This has a performance
@@ -460,6 +461,7 @@ public final class Parcel {
         if (res == null) {
             res = new Parcel(0);
         } else {
+            res.mRecycled = false;
             if (DEBUG_RECYCLE) {
                 res.mStack = new RuntimeException();
             }
@@ -473,7 +475,15 @@ public final class Parcel {
      * the object after this call.
      */
     public final void recycle() {
-        if (DEBUG_RECYCLE) mStack = null;
+        if (mRecycled) {
+            Log.w(TAG, "Recycle called on unowned Parcel. (recycle twice?)", mStack);
+        }
+        mRecycled = true;
+
+        // We try to reset the entire object here, but in order to be
+        // able to print a stack when a Parcel is recycled twice, that
+        // is cleared in obtain instead.
+
         mClassCookies = null;
         freeBuffer();
 
@@ -3546,6 +3556,7 @@ public final class Parcel {
         if (res == null) {
             res = new Parcel(obj);
         } else {
+            res.mRecycled = false;
             if (DEBUG_RECYCLE) {
                 res.mStack = new RuntimeException();
             }
@@ -3593,7 +3604,8 @@ public final class Parcel {
     @Override
     protected void finalize() throws Throwable {
         if (DEBUG_RECYCLE) {
-            if (mStack != null) {
+            // we could always have this log on, but it's spammy
+            if (!mRecycled) {
                 Log.w(TAG, "Client did not call Parcel.recycle()", mStack);
             }
         }
